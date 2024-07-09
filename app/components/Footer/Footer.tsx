3595af33751674aa515beed0654e453087e8f108
Sujal <web.sujal@gmail.com>
2024-07-09 22:05:07 +0530

    feat(footer): add responsiveness to footer

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -8,126 +8,130 @@ const Footer = () => {
       {/* top */}
       <div className={styles.topContainer}>
         <div className={styles.wrapper}>
-          {/* Company Info */}
-          <div className={styles.linksContainer}>
-            <h5 className={styles.linkHeading}>Company Info</h5>
+          <div className={styles.colLinks}>
+            {/* Company Info */}
+            <div className={styles.linksContainer}>
+              <h5 className={styles.linkHeading}>Company Info</h5>
 
-            <div className={styles.linksWrapper}>
-              <Link href="#" className={styles.link}>
-                About Us
-              </Link>
-              <Link href="#" className={styles.link}>
-                Carrier
-              </Link>
-              <Link href="#" className={styles.link}>
-                We are hiring
-              </Link>
-              <Link href="#" className={styles.link}>
-                Blog
-              </Link>
+              <div className={styles.linksWrapper}>
+                <Link href="#" className={styles.link}>
+                  About Us
+                </Link>
+                <Link href="#" className={styles.link}>
+                  Carrier
+                </Link>
+                <Link href="#" className={styles.link}>
+                  We are hiring
+                </Link>
+                <Link href="#" className={styles.link}>
+                  Blog
+                </Link>
+              </div>
+            </div>
+
+            {/* Legal */}
+            <div className={styles.linksContainer}>
+              <h5 className={styles.linkHeading}>Legal</h5>
+
+              <div className={styles.linksWrapper}>
+                <Link href="#" className={styles.link}>
+                  About Us
+                </Link>
+                <Link href="#" className={styles.link}>
+                  Carrier
+                </Link>
+                <Link href="#" className={styles.link}>
+                  We are hiring
+                </Link>
+                <Link href="#" className={styles.link}>
+                  Blog
+                </Link>
+              </div>
+            </div>
+
+            {/* features */}
+            <div className={styles.linksContainer}>
+              <h5 className={styles.linkHeading}>Features</h5>
+
+              <div className={styles.linksWrapper}>
+                <Link href="#" className={styles.link}>
+                  Business Marketing
+                </Link>
+                <Link href="#" className={styles.link}>
+                  User Analytic
+                </Link>
+                <Link href="#" className={styles.link}>
+                  Live Chat
+                </Link>
+                <Link href="#" className={styles.link}>
+                  Unlimited Support
+                </Link>
+              </div>
             </div>
           </div>
 
-          {/* Legal */}
-          <div className={styles.linksContainer}>
-            <h5 className={styles.linkHeading}>Legal</h5>
+          <div className={styles.colLinks}>
+            {/* Resources */}
+            <div className={styles.linksContainer}>
+              <h5 className={styles.linkHeading}>Resources</h5>
 
-            <div className={styles.linksWrapper}>
-              <Link href="#" className={styles.link}>
-                About Us
-              </Link>
-              <Link href="#" className={styles.link}>
-                Carrier
-              </Link>
-              <Link href="#" className={styles.link}>
-                We are hiring
-              </Link>
-              <Link href="#" className={styles.link}>
-                Blog
-              </Link>
+              <div className={styles.linksWrapper}>
+                <Link href="#" className={styles.link}>
+                  IOS & Android
+                </Link>
+                <Link href="#" className={styles.link}>
+                  Watch a Demo
+                </Link>
+                <Link href="#" className={styles.link}>
+                  Customers
+                </Link>
+                <Link href="#" className={styles.link}>
+                  API
+                </Link>
+              </div>
             </div>
-          </div>
 
-          {/* features */}
-          <div className={styles.linksContainer}>
-            <h5 className={styles.linkHeading}>Features</h5>
+            {/* Get In Touch */}
+            <div className={styles.linksContainer}>
+              <h5 className={styles.linkHeading}>Get In Touch</h5>
 
-            <div className={styles.linksWrapper}>
-              <Link href="#" className={styles.link}>
-                Business Marketing
-              </Link>
-              <Link href="#" className={styles.link}>
-                User Analytic
-              </Link>
-              <Link href="#" className={styles.link}>
-                Live Chat
-              </Link>
-              <Link href="#" className={styles.link}>
-                Unlimited Support
-              </Link>
-            </div>
-          </div>
+              <div className={styles.linksWrapper}>
+                <Link href="#" className={styles.linkIconContainer}>
+                  <Image
+                    src="/assets/contact.svg"
+                    alt="phone-icon"
+                    className={styles.icon}
+                    height={24}
+                    width={24}
+                  />
+                  <p className={styles.link}>{"(480) 555-0103"}</p>
+                </Link>
 
-          {/* Resources */}
-          <div className={styles.linksContainer}>
-            <h5 className={styles.linkHeading}>Resources</h5>
+                <Link href="#" className={styles.linkIconContainer}>
+                  <Image
+                    src="/assets/location.svg"
+                    alt="location-icon"
+                    className={styles.icon}
+                    height={24}
+                    width={24}
+                  />
+                  <p className={styles.link}>
+                    4517 Washington Ave. Manchester, <br />
+                    Kentucky 39495
+                  </p>
+                </Link>
 
-            <div className={styles.linksWrapper}>
-              <Link href="#" className={styles.link}>
-                IOS & Android
-              </Link>
-              <Link href="#" className={styles.link}>
-                Watch a Demo
-              </Link>
-              <Link href="#" className={styles.link}>
-                Customers
-              </Link>
-              <Link href="#" className={styles.link}>
-                API
-              </Link>
-            </div>
-          </div>
-
-          {/* Get In Touch */}
-          <div className={styles.linksContainer}>
-            <h5 className={styles.linkHeading}>Get In Touch</h5>
-
-            <div className={styles.linksWrapper}>
-              <Link href="#" className={styles.linkIconContainer}>
-                <Image
-                  src="/assets/contact.svg"
-                  alt="phone-icon"
-                  className={styles.icon}
-                  height={24}
-                  width={24}
-                />
-                <p className={styles.link}>{"(480) 555-0103"}</p>
-              </Link>
-
-              <Link href="#" className={styles.linkIconContainer}>
-                <Image
-                  src="/assets/location.svg"
-                  alt="location-icon"
-                  className={styles.icon}
-                  height={24}
-                  width={24}
-                />
-                <p className={styles.link}>
-                  4517 Washington Ave. Manchester, <br />
-                  Kentucky 39495
-                </p>
-              </Link>
-
-              <Link href="#" className={styles.linkIconContainer}>
-                <Image
-                  src="/assets/email.svg"
-                  alt="email-icon"
-                  className={styles.icon}
-                  height={24}
-                  width={24}
-                />
-                <p className={styles.link}>debra.holt@example.com</p>
-              </Link>
+                <Link href="#" className={styles.linkIconContainer}>
+                  <Image
+                    src="/assets/email.svg"
+                    alt="email-icon"
+                    className={styles.icon}
+                    height={24}
+                    width={24}
+                  />
+                  <p className={styles.link}>debra.holt@example.com</p>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <Product />
       <Pricing />
       <Contact />
-      {/* <Footer /> */}
+      <Footer />
     </main>
   );
 }
