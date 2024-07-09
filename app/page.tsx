43a706fcdf78d985edccee2372aa018383d0a4c0
Sujal <web.sujal@gmail.com>
2024-07-09 20:17:28 +0530

    feat: add responsiveness to Products section

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,9 @@ export default function Home() {
     <main className="">
       <Hero />
       <Product />
-      <Pricing />
-      <Contact />
-      <Footer />
+      {/* <Pricing /> */}
+      {/* <Contact /> */}
+      {/* <Footer /> */}
     </main>
   );
 }
